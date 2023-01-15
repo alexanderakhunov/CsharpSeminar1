6bd439a7f84ea7e7fe7b2941569fc7294e0ceb19
ALEXANDRO <sancheek240@mail.ru>
2023-01-15 21:24:26 +0300

    Add programm SQR

---
+++ SQR/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: "); // Вывод в консоль сообщения
+int number = Convert.ToInt32(Console.ReadLine()); // int.Parse || Convert.ToInt32()
+int sqr = number * number;
+System.Console.WriteLine($"Квадрат числ: {sqr}");
