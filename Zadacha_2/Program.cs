ae14ae674d3a0c6f2e99f9ec6e000149845c0c53
AlexanderAkh <sancheek240@mail.ru>
2023-01-16 10:01:26 +0300

    Add Zadacha_2

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите число дня недели ");
+int NumberWeek = Convert.ToInt32(Console.ReadLine());
+ if (NumberWeek == 1)
+ {
+    Console.Write("Понедельник");
+ }
+ if (NumberWeek == 2)
+ {
+    Console.Write("Вторник");
+ }
+ if (NumberWeek == 3)
+ {
+    Console.Write("Среда");
+ }
+ if (NumberWeek == 4)
+ {
+    Console.Write("Четверг");
+ }
+ if (NumberWeek == 5)
+ {
+    Console.Write("Пятница");
+ }
+ if (NumberWeek == 6)
+ {
+    Console.Write("Суббота");
+ }
+ if (NumberWeek == 7)
+ {
+    Console.Write("Воскресенье");
+ }
+ if (NumberWeek > 7)
+ {
+    Console.Write("В неделе 7 дней");
+ }
