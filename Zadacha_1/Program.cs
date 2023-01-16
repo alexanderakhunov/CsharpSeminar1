26a34ef419d68e4230ca904c3e843ca73b53b12c
AlexanderAkh <sancheek240@mail.ru>
2023-01-16 09:49:12 +0300

    Add Zadacha_1

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите ваше число 1: ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите ваше число 2: ");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+if  (FirstNumber / SecondNumber == SecondNumber)
+    {
+        Console.Write("Является квадратом");
+    }
+    else
+    {
+     Console.Write("Не является квадратом");
+    };
